9b2dc179a185db0e43cad6ebc1cec1c8f2c65d86
David Schoenholtz <dmschoenholtz@gmail.com>
2019-07-10 18:22:10 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
+class Driver{
+  constructor(name, startDate){
+    this.name = name;
+    this.startDate = new Date(startDate);
+  }
 
+  yearsExperienceFromBeginningOf(year){
+
+  }
+
+}
+
+class Route{
+  constructor(beginningLocation, endingLocation){
+    
+  }
+
+}
